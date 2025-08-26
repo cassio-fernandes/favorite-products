8ead02e978f8c7c364530999679d8733c6383cb4
Cássio F <cassiofernandes@protonmail.ch>
2025-08-25 22:28:23 -0300

    #10 create model favorite_products

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,31 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from clients.models import Client
+
+
+class FavoriteProducts(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    client = models.ForeignKey(
+        Client,
+        null=False,
+        blank=False,
+        verbose_name='Cliente',
+        on_delete=models.CASCADE,
+        related_name="client"
+    )
+    product_id = models.IntegerField(
+        null=False,
+        verbose_name='Produto',
+    )
+
+    class Meta:
+        db_table = "favorite_products"
+        verbose_name = "Produto favorito"
+        verbose_name_plural = "Produtos favoritos"
+        unique_together = ['client', 'product_id']
